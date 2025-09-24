6eb8a10f0c74ec54df5baf95724fb85863595735
seojin23 <sand6@naver.com>
2025-09-24 12:26:37 +0900

    docs: homePage 내용 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default async function homePage() {
   return (
     <div>
-      <h1>Welcome Home</h1>
-      <p>반갑습니다</p>
+      <h1 className="text-2xl font-bold mb-5">Welcome Home</h1>
+      <p>1. clerk authentication</p>
     </div>
   )
 }
